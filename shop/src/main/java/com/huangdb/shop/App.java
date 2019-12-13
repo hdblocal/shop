ebd348c3e71c810945839f849a47dd921b253557
hdblocal <454665976@qq.com>
2019-12-13 15:52:51 +0800

    Update App.java

--- shop/src/main/java/com/huangdb/shop/App.java
+++ shop/src/main/java/com/huangdb/shop/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * Hello world!
+ * Hello world !
  *
  */
 @SpringBootApplication
